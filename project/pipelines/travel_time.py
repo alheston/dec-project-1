1fa1600a220c37b8dbba2f2ea6bded16ed8a5eaa
Josh Botticher <jjbotticher@outlook.com>
2024-05-28 16:36:18 -0400

    updates

---
+++ project/pipelines/travel_time.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+from connectors.travel_time_api import TravelTimeApiClient
+from assets.travel_time import extract_travel_time
+# from sqlalchemy import Table, MetaData, Column, Integer, String, Float
+
+
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    API_KEY = os.environ.get("API_KEY")
+    APP_ID = os.environ.get("APP_ID")
+    DB_USERNAME = os.environ.get("DB_USERNAME")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    SERVER_NAME = os.environ.get("SERVER_NAME")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME")
+    PORT = os.environ.get("PORT")
+
+    travel_time_api_client = TravelTimeApiClient(api_key = API_KEY,app_id = APP_ID)
+    response_data = travel_time_api_client.get_data(type="driving")
+    df_travel_time = extract_travel_time(response_data=response_data)
